d22af6ab1bd374f1b4c76ce9759e13c7fff6c9f1
Victorthomas <77334924+unpseudonormal@users.noreply.github.com>
2023-03-28 19:17:48 +0200

    Update projetpython.py

--- projetpython.py
+++ projetpython.py
@@ -1,19 +1,17 @@
 import pandas as pd
 import plotly.express as px
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 
-# Replace these dummy data with your actual time series data.
-data = {
-    "date": pd.date_range(start="2022-01-01", periods=60, freq="D"),
-    "price": [10000 + i * 50 for i in range(60)],
-}
-
-df = pd.DataFrame(data)
+df = pd.read_csv("bnp_paribas_prices.csv", parse_dates=["Timestamp"])
 
 app = dash.Dash(__name__)
+
+app.layout = html.Div([html.H1("BNP Paribas Price Dashboard"),dcc.Graph(id="time-series-graph"),dcc.Interval(id="interval-component", interval=1 * 1000,  n_intervals=0),] )
+
+
 
 app.layout = html.Span(
     [
@@ -26,8 +24,8 @@ app.layout = html.Span(
 @app.callback(Output("time-series-graph", "figure"), Input("interval-component", "n_intervals"))
 def update_graph(n):
     df_subset = df.iloc[:n+1, :]
-    fig = px.line(df_subset, x="date", y="price")
+    fig = px.line(df_subset, x="Timestamp", y="Price")
     return fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0')
